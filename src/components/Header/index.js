81011d759bc0fe5c9c9929688e19ac2cc9ad9751
Skyler R <skyler.rencher@gmail.com>
2021-01-16 19:15:58 -0500

    added code to the footer and added some css styling to index.css.

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../index.css";
+
+function Header () {
+    return (
+        <header className="jumbotron jumbotron-fluid head-jumbo">
+            <div>
+                <h1>Personnel Directory</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
