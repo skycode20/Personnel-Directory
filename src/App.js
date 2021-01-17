12eb96f47c7ee4455b58b38e27f0c03b78ce5e62
Skyler R <skyler.rencher@gmail.com>
2021-01-16 19:29:41 -0500

    added tje header and footer components to the App.js.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
 
   return (
     <div className="App">
-      <p>Personnel Directory</p>
+      <Header />
+
+      <h1>Personnel Directory</h1>
+      
+      <Footer />
     </div>
   );
   
