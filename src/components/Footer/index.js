e2910e2a963260475eec45717b3b91fde56f4bcf
Skyler R <skyler.rencher@gmail.com>
2021-01-16 19:18:32 -0500

    added a footer component.

---
+++ src/components/Footer/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../../index.css";
+
+function Footer () {
+	return (
+		<footer class="footer">
+
+			<div class="container-fluid h-25">
+
+				<div class="row">
+					<div class="col-lg-12 bg-light text-center text-bottom text-muted pt-3">
+						<p>Copyright © Skyler Rencher 2021</p>
+					</div>
+
+
+				</div>
+
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
