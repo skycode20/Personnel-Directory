a1cf644c20325b88c9a9dd3618522a11d41c000c
Skyler R <skyler.rencher@gmail.com>
2021-01-16 19:38:32 -0500

    added a main component with some basic code to start.

---
+++ src/components/Main/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../index.css";
+
+function Main () {
+    return (
+        <main className="main-area">
+            Stuff goes here:
+        </main>
+    )
+}
+
+export default Main;
